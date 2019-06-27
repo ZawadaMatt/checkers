87b7b531df1a5dfea6c1534ca122e9e46018a22b
Mateusz Zawada <mateusz.zawada10@gmail.com>
2019-06-27 18:07:21 +0200

    first

---
+++ src/main/java/com.github.zawadamatt/Grid.java
@@ -0,0 +1,6 @@
+package com.github.zawadamatt;
+
+public class Grid {
+
+
+}
---
+++ src/main/java/com.github.zawadamatt/Main.java
@@ -0,0 +1,9 @@
+package com.github.zawadamatt;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("D");
+    }
+
+}
